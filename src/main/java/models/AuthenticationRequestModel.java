3c88914f06171cc73762fa5a23ae42fc1cb2829f
PopovMarina <pilgrim7474@gmail.com>
2024-03-21 21:22:59 +0200

    Lesson 10

---
+++ src/main/java/models/AuthenticationRequestModel.java
@@ -0,0 +1,35 @@
+package models;
+
+public class AuthenticationRequestModel {
+    private String userName;
+    private String password;
+
+    public AuthenticationRequestModel(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public static AuthenticationRequestModel userName(String userName) {
+        return new AuthenticationRequestModel(userName, null);
+    }
+    public  AuthenticationRequestModel password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
